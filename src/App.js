c3575ed7ff4b4bc50b3d0c6b37a449e2e7844114
Rohaan Hussain <syed.hussain@agoda.com>
2021-10-26 08:08:02 +0500

    add trevis

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 2
         </a>
       </header>
     </div>
